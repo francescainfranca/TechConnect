e683c06089d63dbdc2101b7194e9f4fca9131645
David Angelo <54418832+empCarnivorePewPew@users.noreply.github.com>
2021-03-02 14:59:47 -0600

    remove unnecessary js

--- webpages.js
+++ webpages.js
@@ -1,15 +1,3 @@
-
-var navOpened=false;
-document.onclick = function(){
-    if(!navOpened){
-        closeNav();
-    }
-    else{
-        navOpened=false
-    }
-}
-
-
 function openNav() {
 document.getElementById("myNav").style.width = "100%";
     navOpened=true;
