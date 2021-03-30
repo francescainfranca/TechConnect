98ea3ec72ef6787724ba1446e3220e80d1a22378
dipikaxsharma <s530527@nwmissouri.edu>
2021-03-30 11:29:08 -0500

    Routes: adding user

---
+++ backend/routes/user.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+let User = require('../models/user.model');
+
+router.route('/').get((req, res) => {
+   User.find() // Mongoose method: gets data
+      .then(users => res.json(users)) // res.json: returns in json format from database
+      .catch(err => res.status(400).json('Error: ' + err)); //error message
+   
+});
+
+router.route('/add').post((req, res) => {
+   const username = req.body.username; 
+   const newUser = new User({username});
+
+   newUser.save() //Saved to MongoDB database
+      .then(() => res.json('User added!')) 
+      .catch(err => res.status(400).json('Error: ' + err));
+});
+module.exports = router; //exporting the router
