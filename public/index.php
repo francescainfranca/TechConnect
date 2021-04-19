d7077ded373fa5a33f5e38f32780346e2eac19a0
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2021-04-19 03:57:44 -0500

    2

--- public/index.php
+++ public/index.php
@@ -1,24 +1,24 @@
 <?php
 
-$serverName = "localhost";
-$dBUsername = "root";
-$dBPassword = "";
-$dbName = "phpproject01"; 
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "phpproject01"; 
 
-mysql_connect($host,$user, $password);
+mysql_connect($host,$user,$password);
 mysql_select_db($dbName)
 
 if(isset(POST['username'])){
-	$uname=$POST['username'];
+	$uname=$_POST['username'];
 	$password=$_POST['password'];
 
-	$sql="select * from loginform where username='".$uname."'AND Pass='".$password."' 
+	$sql="select * from loginform where user='".$uname."'AND Pass='".$password."' 
 	limit 1";
 
 	$result=mysql_query($sql);
 
 	if(mysql_num_rows($result)==1){
-		echo " You have Logged in";
+		<a href="href=student_dashboard.html">
 		exit();
 	}
 	else{
