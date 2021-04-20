100020db89e2fbfdb9d7d9e4b8d1efe2eec09842
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2021-04-20 01:40:21 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -1,4 +1,5 @@
 <?php
+<<<<<<< HEAD
 // Initialize the session
 session_start();
 
@@ -65,21 +66,55 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
     
     // Close connection
     $mysqli->close();
+=======
+
+$host = "localhost";
+$user = "root";
+$password = "";
+$db = "phpproject01"; 
+
+mysql_connect($host,$user,$password);
+mysql_select_db($db);
+
+if(isset($_POST['username'])){
+	$uname=$_POST['username'];
+	$password=$_POST['password'];
+
+	$sql="select * from loginform where user='".$uname."'AND Pass='".$password."' 
+	limit 1";
+
+	$result=mysql_query($sql);
+
+	if(mysql_num_rows($result)==1){
+		echo"The password is right"
+		exit();
+	}
+	else{
+		echo"You have entered the wrong password"
+		exit();
+	}
+>>>>>>> parent of 41647c3 (Update index.php)
 }
+
 ?>
- 
+
 <!DOCTYPE html>
-<html lang="en">
+<html>
 <head>
-    <meta charset="UTF-8">
-    <title>Login</title>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-    <style>
-        body{ font: 14px sans-serif; }
-        .wrapper{ width: 350px; padding: 20px; }
-    </style>
+	<title>Login Page</title>
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="index.css">
+	<link rel="shortcut icon" href="favicon.ico">
 </head>
 <body>
+<<<<<<< HEAD
     <div class="wrapper">
         <h2>Login</h2>
         <p>Please fill in your credentials to login.</p>
@@ -107,5 +142,53 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
           
         </form>
     </div>
+=======
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<form method = "POST" action="#">
+		<div class="card">
+			<div class="card-header">
+                
+				<h3> <img src="Northwest_Missouri_State_Bearcats_logo.svg" alt="Logo" style="width:60px;height:60px;"> Northwest Log In</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-twitter-square"></i></span>
+				</div>
+			</div>
+			<div class="card-body">
+				<form id="login" action="\" method="post">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="username" id="username" name="username">
+						
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="password" id="password" name="password">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox" id="Remember" name="Remember">Remember Me
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Login" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					<a href="#">Need Help</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+>>>>>>> parent of 41647c3 (Update index.php)
 </body>
 </html>
