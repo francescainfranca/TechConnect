51aa29f99b203b103e3ca84cceb9fed805efab65
Avishek Khadka <42814492+Askay07@users.noreply.github.com>
2021-04-20 12:02:19 -0500

    Update index.php

--- public/index.php
+++ public/index.php
@@ -79,7 +79,7 @@ session_start();
 						<div class="input-group-prepend">
 							<span class="input-group-text"><i class="fas fa-user"></i></span>
 						</div>
-						<input type="text" class="form-control" placeholder="user_name" id="user_name" name="user_name">
+						<input type="text" class="form-control" placeholder="user_name" id="user_name" name="username">
 						
 					</div>
 					<div class="input-group form-group">
